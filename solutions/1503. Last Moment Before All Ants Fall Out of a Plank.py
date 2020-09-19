b4831a9089ced7d36268a24ca1da3b46461b46e6
Tejal Shanbhag <tejalvs14@gmail.com>
2020-09-19 19:51:40 -0400

    160 ms

--- solutions/1503. Last Moment Before All Ants Fall Out of a Plank.py
+++ solutions/1503. Last Moment Before All Ants Fall Out of a Plank.py
@@ -1,20 +1,27 @@
 class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
-        BestRightDistance=0
-        BestLeftDistance=0
         
-        for i in range(0,len(right)):
-            #print(right[i],n,n-right[i])
-            displacement=n-right[i]
-            if (displacement>BestRightDistance):
-                BestRightDistance=displacement
+        if(len(left)==0 ):
+            return abs(n-min(right))
+        elif(len(right)==0 ):
+            return abs(0-max(left))
+        else:
+            maxLeft=max(left)
+            maxRight=min(right)
+        print(maxRight,maxLeft)
+        
+        return(max((abs(n-maxRight)),abs(0-maxLeft)))
+        
+        
+        '''      
+        BestRightDistance=0
+        BestLeftDistance=0
+        
+        for i in range(0,len(right)):
+            #print(right[i],n,n-right[i])
+            displacement=n-right[i]
+            if (displacement>BestRightDistance):
+                BestRightDistance=displacement
        
-        for j in range(0,len(left)):
-            print(left[j],n,n-left[j])
-            displacement=abs(0-left[j])
-            if (displacement>BestLeftDistance):
-                BestLeftDistance=displacement
-    
-    
-    
-        return max(BestRightDistance,BestLeftDistance)
+        for j in range(0,len(left)):
+            print(left[j],n,n-left[j])
