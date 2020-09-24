bf2787916083992a7c177022283680df02e5ad72
Tejal Shanbhag <tejalvs14@gmail.com>
2020-09-24 13:19:31 -0400

    124 ms

---
+++ solutions/451. Sort Characters By Frequency.py
@@ -0,0 +1,25 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+      
+        spltStr=list(s)
+        spltStr.sort()
+        freq={}
+        for i in spltStr:
+            if i in freq.keys():
+                freq[i]+=1
+            else:
+                freq[i]=1
+        result=''
+        sorted_dict = dict(sorted(freq.items(), key=operator.itemgetter(1),reverse=True))
+        print(sorted_dict)
+        for i in sorted_dict.keys():
+            count=sorted_dict[i]
+            for j in range(0,count):
+                result=result+i
+        print(result)
+        return(result)
+       
+​
+                
+            
+                
