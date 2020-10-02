cdb8e3e065aeb872543b3a6162aea936b3574248
Tejal Shanbhag <tejalvs14@gmail.com>
2020-10-02 01:08:43 -0400

    84 ms

--- solutions/26. Remove Duplicates from Sorted Array.py
+++ solutions/26. Remove Duplicates from Sorted Array.py
@@ -1,15 +1,24 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        
         if len(nums) == 0: return 0
         if len(nums) == 1: return 1
-        
-        # nums = [0,0,1,1,1,2,2,3,3,4]
-        j = 1 # slover pointer, only move when meet unique number
-        for i in range(1, len(nums)): # faster pointer, i will iterate over all element in nums
-            if nums[i] != nums[i-1]: # when nums[i] is a unique number, assign it to nums[j]
+        j = 1 
+        for i in range(1, len(nums)): 
+            if nums[i] != nums[i-1]: 
                 nums[j] = nums[i]
                 j += 1
-        # after for loop, i = 9, j = 5, nums = [0, 1, 2, 3, 4, 2, 2, 3, 3, 4]
-        # we can see the nums[:5] is the unique number list we want 
+      
         return j
+        '''j = 1
+        i = 0
+        while(j<len(arr)):
+            if arr[i] == arr[j]:
+                j+=1
+            else:
+                i+=1
+                arr[i] = arr[j]
+                j+=1
+        arr = arr[0:i+1]
+        return i+1'''
       
