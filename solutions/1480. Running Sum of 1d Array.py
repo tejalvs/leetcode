7ef36dcca1f43806988c8dfe07534424c698ee95
Tejal Shanbhag <tejalvs14@gmail.com>
2020-10-01 23:02:05 -0400

    40 ms

---
+++ solutions/1480. Running Sum of 1d Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        print(nums)
+        result=[]
+        sum=0
+        for i in range(len(nums)):
+            sum+=nums[i]
+            result.append(sum)
+        return result
