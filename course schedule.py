4dd03c85709e30994e528aa7d7b45dd7094281f9
Tejal Shanbhag <tejalvs14@gmail.com>
2020-10-07 02:52:30 -0400

    Add files via upload

---
+++ course schedule.py
@@ -0,0 +1,43 @@
+def courseSchedule(course):
+    course_Map=generateCourseMap(course)
+    keylist=course_Map.keys()
+    result={}
+    visited=[]
+    for i in keylist:
+        start=i
+        end=''
+        temp=course_Map[i]
+        visited.append(i)
+        while (len(temp)!=0):
+            currentkey=temp.pop(0)
+            if currentkey in visited:
+                continue
+            visited.append(currentkey)
+            if currentkey in keylist:
+                temp=temp+course_Map[currentkey]
+            elif currentkey not in keylist:
+                end=currentkey
+                if start in result.keys():
+                    result[start].append(end)
+                else:
+                    result[start]=[end]
+    return result
+
+def generateCourseMap(course):
+    course_Map = {}
+    for i in course:
+        if i[0] in course_Map.keys():
+            temp = course_Map[i[0]]
+            temp.append(i[1])
+            course_Map[i[0]] = temp
+        else:
+            course_Map[i[0]] = [i[1]]
+    return course_Map
+
+def Main():
+    course=[['A','B'],['A','C'],['B','K'],['C','K'],['L','M'],
+                ['L','N'],['D','E'],['D','J'],['E','F'],['E','G'],['F','H'],['G','I']]
+    result=courseSchedule(course)
+    print(result)
+
+Main()
