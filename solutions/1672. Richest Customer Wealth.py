2a76a1d06220ac58187caee062b8c6498d649e9e
Tejal Shanbhag <tejalvs14@gmail.com>
2020-12-08 09:57:08 -0500

    56 ms

--- solutions/1672. Richest Customer Wealth.py
+++ solutions/1672. Richest Customer Wealth.py
@@ -1,12 +1,17 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        highest=0
-        for acc in accounts:
-            currentSum=0
-            for i in acc:
-                currentSum=currentSum+i
-            if currentSum> highest:
-                highest=currentSum
-        return highest
+        return max([sum(item) for item in accounts]);
+        '''highest=0
+        for acc in accounts:
+            currentSum=0
+            for i in acc:
+                currentSum=currentSum+i
+            if currentSum> highest:
+                highest=currentSum
+        return highest'''
+        
+        
+        
+        
         
         
