13f503959fcab6e8d089afb4f356deb160a80769
Tejal Shanbhag <tejalvs14@gmail.com>
2020-12-12 02:32:20 -0500

    60 ms

---
+++ solutions/1470. Shuffle the Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        output = []
+        
+        for i in range(n):
+            output.append( nums[i])
+            output.append( nums[n+i]  )
+            
+        return output
