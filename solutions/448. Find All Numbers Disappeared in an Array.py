d2d7538b2b023911bc9801019b9c66fa971c24f4
Tejal Shanbhag <tejalvs14@gmail.com>
2021-01-11 19:03:43 -0500

    344 ms

--- solutions/448. Find All Numbers Disappeared in an Array.py
+++ solutions/448. Find All Numbers Disappeared in an Array.py
@@ -1,10 +1,15 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        maps={}
-        result=[]
-        for number in nums:
-            maps[number]=1
-        for i in range(1,len(nums)+1):
-            if i not in maps.keys():
-                result.append(i)
-        return result
+        '''maps={}
+        result=[]
+        for number in nums:
+            maps[number]=1
+        for i in range(1,len(nums)+1):
+            if i not in maps.keys():
+                result.append(i)
+        return result'''
+        all_nums_set = set(range(1,len(nums)+1))
+        set_nums_set = set(nums)
+        res = all_nums_set.difference(set_nums_set)
+        return list(res)
+        
